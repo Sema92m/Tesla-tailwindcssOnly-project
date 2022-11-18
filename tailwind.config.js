eb9c25ab93f37a2f0d018477fdf0f59dec7e7fa8
Simon <sema92m@wp.pl>
2022-11-18 23:42:09 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./public/**/*.html"],
+    darkmode: "class",
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
